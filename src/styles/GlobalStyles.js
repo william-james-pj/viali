e66be7f17f0a1c2cc62e2ae40a90220ee6da387e
William James <william.james.pj@gmail.com>
2021-05-08 19:00:38 -0300

    add font style

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,6 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 import * as color from '../config/colors';
 
 export default createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
     * {
         margin: 0;
         padding: 0;
