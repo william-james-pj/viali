53a9c71bbe4eadc430501df3cd37beb3f35fc4a7
William James <william.james.pj@gmail.com>
2021-05-13 11:14:16 -0300

    update in component loading

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import * as colors from '../../config/colors';
 
-import { useLoading, BallTriangle } from '@agney/react-loading';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { LoaderContainer } from './styles';
 
-function Loading({ active }) {
-  const { containerProps, indicatorEl } = useLoading({
-    loading: active,
-    loaderProps: {
-      style: { color: colors.colorGreen1 },
-    },
-    indicator: <BallTriangle width="50" />,
-  });
-
-  return <LoaderContainer {...containerProps}>{indicatorEl}</LoaderContainer>;
+function Loading() {
+  return (
+    <LoaderContainer>
+      <CircularProgress style={{ color: colors.colorGreen1 }} />
+    </LoaderContainer>
+  );
 }
-
-Loading.propTypes = {
-  active: PropTypes.bool.isRequired,
-};
 
 export default Loading;
