5f0ba171d3113586a259f09c980cb5ee0b4191aa
William James <william.james.pj@gmail.com>
2021-05-16 09:52:42 -0300

    Update design layout

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,11 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { db, storage } from '../../services/firestore';
 
 import MyHeader from '../../components/MyHeader/index';
 import ImgPath from '../../assets/img/welcome.svg';
+import Loading from '../../components/Loading/index';
 
-import { Container, Text, TextContainer } from './styles';
+import {
+  Container,
+  TextContainer,
+  BoxContainer,
+  Box,
+  ImgBox,
+  TextBox,
+  Img,
+  Override,
+} from './styles';
 
 function Home() {
+  const [isLoading, setLoading] = useState(true);
+  const [isImgLoading, setImgLoading] = useState(true);
+  const [firebaseData, setFirebaseData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      let items = [];
+      const response = db.collection('Home');
+      const data = await response.get();
+      await data.docs.forEach((documentSnapshot) => {
+        items.push({
+          text: documentSnapshot.data().text,
+          img: documentSnapshot.data().img,
+        });
+      });
+      await fetchImg(items);
+      setFirebaseData(items);
+      setLoading(false);
+    } catch (error) {
+      setLoading(true);
+      console.log(error);
+    }
+  };
+
+  const fetchImg = async (items) => {
+    try {
+      await Promise.all(
+        items.map(async (item) => {
+          let starsRef = storage.ref('Home/' + item.img);
+          await starsRef
+            .getDownloadURL()
+            .then((url) => {
+              item.url = url;
+            })
+            .catch((error) => {
+              console.log(error);
+              return null;
+            });
+        }),
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading)
+    return (
+      <Container>
+        <Loading active={isLoading} />
+      </Container>
+    );
+
   return (
     <Container>
       <MyHeader
@@ -13,14 +80,26 @@ function Home() {
         text={'ViALi'}
         imgPath={ImgPath}
       />
-      <TextContainer>
-        <Text>
-          {`ViALi é um site onde você irá\n
-        encontar Jogos, Podcasts e 
-        Mapas Mentais sobre alguns 
-        temas biológios!`}
-        </Text>
-      </TextContainer>
+      <TextContainer>O que é a ViALi?</TextContainer>
+      <BoxContainer>
+        {firebaseData.length > 0 &&
+          firebaseData.map((item, key) => {
+            return (
+              <Box key={key}>
+                {isImgLoading && <Loading active={isImgLoading} />}
+                <ImgBox>
+                  <Img
+                    src={item.url}
+                    style={{ display: isImgLoading ? 'none' : 'block' }}
+                    onLoad={() => setImgLoading(false)}
+                  />
+                </ImgBox>
+                <TextBox>{item.text}</TextBox>
+                <Override></Override>
+              </Box>
+            );
+          })}
+      </BoxContainer>
     </Container>
   );
 }
--- src/views/Home/styles.js
+++ src/views/Home/styles.js
@@ -8,42 +8,86 @@ export const Container = styled.div`
   height: 100%;
 
   padding: 30px;
-  overflow: hidden;
+  overflow-x: hidden;
 
   position: relative;
 `;
 
 export const TextContainer = styled.div`
   width: 100%;
+  margin-top: 30px;
+  color: ${color.blackText};
+  font-size: ${fonts.normal};
+  font-weight: 700;
+`;
+
+export const BoxContainer = styled.div`
+  width: 100%;
   display: flex;
+  flex-wrap: wrap;
   align-items: center;
+  justify-content: space-between;
   margin-top: 30px;
 
   ${respondToDown.md`
     justify-content: center;
-    text-align: center;
-  `}
+    margin-bottom: 30px;
+  `};
 `;
 
-export const Text = styled.div`
-  color: ${color.blackText};
-  font-size: ${fonts.normal};
-  line-height: 1.8;
-  width: 220px;
+export const Box = styled.div`
+  width: 250px;
+  height: 300px;
+  border-radius: 10px;
+  background: ${color.white};
+  box-shadow: 0px 4px 4px 2px rgba(0, 0, 0, 0.25);
+
+  position: relative;
+  overflow: hidden;
+
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 1fr;
+  gap: 0px 0px;
+  grid-template-areas:
+    'ImgBox'
+    'TextBox';
+
+  padding: 10px;
 
   ${respondToDown.md`
-    width: 80%;
+    margin-bottom: 30px;
   `}
 `;
 
-export const Circle = styled.div`
-  width: 400px;
-  height: 400px;
-  border-radius: 50%;
+export const ImgBox = styled.div`
+  grid-area: ImgBox;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+`;
 
+export const TextBox = styled.div`
+  grid-area: TextBox;
+  display: flex;
+  align-items: center;
+  color: ${color.blackText};
+  font-size: ${fonts.sm};
+  text-align: center;
+`;
+
+export const Img = styled.img`
+  width: auto;
+  height: 100%;
+`;
+
+export const Override = styled.div`
+  width: 100%;
+  height: 100%;
+  background: #000;
+  opacity: 0.1;
+  z-index: 2;
   position: absolute;
-  bottom: -100px;
-  right: -100px;
-
-  background: ${color.colorGreen2};
+  top: 0;
 `;
